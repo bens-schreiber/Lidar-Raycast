4c5f4a749f4bf86fedcbc985548d75858a3ee66a
Ben Schreiber <bpschreiber2003@gmail.com>
2023-12-23 22:59:54 -0800

    Seperated impl

--- src/bvh/bvh_build_impl.c
+++ src/bvh/bvh_build_impl.c
@@ -67,28 +67,3 @@ BVH_Node *build_bvh_tree_impl(Renderable *renderable, size_t renderable_size, Bo
 
     return new_node;
 }
-
-BVH_Tree *build_bvh_tree(struct Renderable *renderable, size_t renderable_size, struct BoundingBox scene_aabb)
-{
-    BVH_Tree *tree = malloc(sizeof(BVH_Tree));
-    tree->root = build_bvh_tree_impl(renderable, renderable_size, scene_aabb);
-    return tree;
-}
-
-void draw_bvh_tree_impl(BVH_Node *node)
-{
-    if (node == NULL)
-    {
-        return;
-    }
-
-    DrawBoundingBox(node->bounding_box, BLACK);
-
-    draw_bvh_tree_impl(node->left);
-    draw_bvh_tree_impl(node->right);
-}
-
-void draw_bvh_tree(BVH_Tree *tree)
-{
-    draw_bvh_tree_impl(tree->root);
-}
---
+++ src/bvh/bvh_tree.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "raylib.h"
+#include "../renderable/renderable.h"
+#include "bvh.h"
+
+BVH_Node *build_bvh_tree_impl(Renderable*, size_t, BoundingBox);
+
+BVH_Tree *build_bvh_tree(Renderable *renderable, size_t renderable_size, BoundingBox scene_aabb)
+{
+    BVH_Tree *tree = malloc(sizeof(BVH_Tree));
+    tree->root = build_bvh_tree_impl(renderable, renderable_size, scene_aabb);
+    return tree;
+}
+
+void draw_bvh_tree_impl(BVH_Node *node)
+{
+    if (node == NULL)
+    {
+        return;
+    }
+
+    DrawBoundingBox(node->bounding_box, BLACK);
+
+    draw_bvh_tree_impl(node->left);
+    draw_bvh_tree_impl(node->right);
+}
+
+void draw_bvh_tree(BVH_Tree *tree)
+{
+    draw_bvh_tree_impl(tree->root);
+}
